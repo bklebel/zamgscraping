270e414d1c0f42413c0dcf0adb29116991c8c1ee
Benjamin <bklebel@github.com>
2021-09-29 22:11:16 +0200

    cleaning up and adding documentation

--- fun_scrape.py
+++ fun_scrape.py
@@ -1,17 +1,36 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-# from urllib.error import URLError
 import urllib
+import sys
 import re
 import time
 import logging
+
 logger = logging.getLogger()
 
 
-def read_page(site, scrapefun, **kwargs):
+class recursionlimit:
+    """context manager to set a specific recursion limit for a piece of code
+    we are using this here, in order to additionally limit the recursions
+    when reading a page which fails because of a network issue
+    without this, we write a lot of logfiles as errors, especially if
+    we are disconnected from the network for extended periods"""
 
-    req = Request(site,  headers={
-                  'User-Agent': 'Mozilla/5.0'})
+    def __init__(self, newlimit):
+        self.limit_inside = newlimit
+        self.limit_outside = sys.getrecursionlimit()
+
+    def __enter__(self):
+        sys.setrecursionlimit(self.limit_inside)
+
+    def __exit__(self, type, value, tb):
+        sys.setrecursionlimit(self.limit_outside)
+
+
+def read_page(site, **kwargs):
+    """read a page, in case of failure, repeat"""
+
+    req = Request(site, headers={"User-Agent": "Mozilla/5.0"})
     try:
         with urlopen(req) as conn:
             page = conn.read()
@@ -19,69 +38,44 @@ def read_page(site, scrapefun, **kwargs):
     except urllib.error.URLError as e:
         time.sleep(0.1)
         logger.exception(e)
-        logging.error('ZamgScraping URLError: {}'.format(e.reason))
-        return scrapefun()
-
-
-# def scr_Wien(page):
-#     temps = dict()
-#     soup = BeautifulSoup(page, features="html.parser")
-#     # print(soup.findAll('class="text_right wert selected"'))
-#     # print(soup)
-#     table = soup.find('table', attrs={'class': 'dynPageTable'})
-#     # print(table)
-
-#     rows = table.findAll('tr', {'class': 'dynPageTableLine1'}) + \
-#         table.findAll('tr', {'class': 'dynPageTableLine2'})
-#     # print('')
-#     for x in rows:
-#         # print(x)
-#         name = x.find('td', {'class': 'wert'}).text
-#         name_corr = name.replace(' ', '_', 5).replace(
-#             'ä', 'ae', 3).replace('ß', 'ss', 3).replace('-', '_', 5)
-#         # print(x.find('td', {'class': 'wert'}).text)
-#         value = float(
-#             x.find('td', {'class': 'text_right wert selected'}).text[:-1])
-#         temps[name_corr] = value
-#         # print(float(x.find('td', {'class': 'text_right wert selected'}).text[:-1]))
-
-#     return temps
-
-
-# def scrape_Wien_temps():
-#     site = 'https://www.zamg.ac.at/cms/de/wetter/wetterwerte-analysen/wien'
-#     page = read_page(site, scrape_Wien_temps)
-#     temps = scr_Wien(page)
-#     return temps
+        logging.error("ZamgScraping URLError: {}".format(e.reason))
+        return read_page(site)
 
 
 def scr_Wien_all(page):
+    """scrape relevant data"""
     data = {}
     soup = BeautifulSoup(page, features="html.parser")
     # print(soup.findAll('class="text_right wert selected"'))
     # print(soup)
-    table = soup.find('table', attrs={'class': 'dynPageTable'})
+    table = soup.find("table", attrs={"class": "dynPageTable"})
     # print(table)
 
-    rows = table.findAll('tr', {'class': 'dynPageTableLine1'}) + \
-        table.findAll('tr', {'class': 'dynPageTableLine2'})
+    rows = table.findAll("tr", {"class": "dynPageTableLine1"}) + table.findAll(
+        "tr", {"class": "dynPageTableLine2"}
+    )
     # print('')
     for x in rows:
         # print(x)
-        name = x.find('td', {'class': 'wert'}).text
-        name_corr = name.replace(' ', '_', 5).replace(
-            'ä', 'ae', 3).replace('ß', 'ss', 3).replace('-', '_', 5)
+        name = x.find("td", {"class": "wert"}).text
+        name_corr = (
+            name.replace(" ", "_", 5)
+            .replace("ä", "ae", 3)
+            .replace("ß", "ss", 3)
+            .replace("-", "_", 5)
+        )
         # print(x.find('td', {'class': 'wert'}).text)
         temperature = float(
-            x.find('td', {'class': 'text_right wert selected'}).text[:-1])
-        humidity = float(x.find('td', {'class': 'text_center wert'}).text[:-2])
+            x.find("td", {"class": "text_right wert selected"}).text[:-1]
+        )
+        humidity = float(x.find("td", {"class": "text_center wert"}).text[:-2])
 
-        w = x.findNext('td', {'class': 'wert text_right'})
+        w = x.findNext("td", {"class": "wert text_right"})
         wind = float(w.text[:-4])
-        p = w.findNext('td', {'class': 'wert text_right'})
+        p = w.findNext("td", {"class": "wert text_right"})
         precipitation = float(p.text[:-3])
-        sun = float(x.find('td', {'class': 'wert text_center'}).text[:-2])
-        press = p.findNext('td', {'class': 'wert text_right'})
+        sun = float(x.find("td", {"class": "wert text_center"}).text[:-2])
+        press = p.findNext("td", {"class": "wert text_right"})
         pressure = float(press.text[:-4])
         data[name_corr] = dict(
             temperature=temperature,
@@ -96,19 +90,13 @@ def scr_Wien_all(page):
     return data
 
 
-def scrape_Wien_all():
-    site = 'https://www.zamg.ac.at/cms/de/wetter/wetterwerte-analysen/wien'
-    page = read_page(site, scrape_Wien_all)
-    data = scr_Wien_all(page)
-    return data
-
-
 def scr_T(page):
+    """scrape the time"""
     soup = BeautifulSoup(page, features="html.parser")
-    string = soup.find(
-        'h2', {'class': 'dynPageTextHead float_left no_margin_bottom'})
+    string = soup.find("h2", {"class": "dynPageTextHead float_left no_margin_bottom"})
     date = re.findall(
-        r'Aktuelle Messwerte der Wetterstationen von ([0-9]{2})', str(string))
+        r"Aktuelle Messwerte der Wetterstationen von ([0-9]{2})", str(string)
+    )
     # print(date[0])
     try:
         d = float(date[0])
@@ -118,55 +106,18 @@ def scr_T(page):
     return d
 
 
+def apply_scraping(site, scrapefun):
+    with recursionlimit(100):
+        page = read_page(site)
+        data = scrapefun(page)
+        return data
+
+
+def scrape_Wien_all():
+    site = "https://www.zamg.ac.at/cms/de/wetter/wetterwerte-analysen/wien"
+    return apply_scraping(site, scr_Wien_all)
+
+
 def scrape_time():
-    site = 'https://www.zamg.ac.at/cms/de/wetter/wetterwerte-analysen/wien'
-    page = read_page(site, scrape_time)
-    d = scr_T(page)
-    return d
-
-
-# def scrape_innerestadt_():
-
-#     site = 'https://www.zamg.ac.at/cms/de/wetter/wetterwerte-analysen/wien'
-
-#     req = Request(site,  headers={
-#                   'User-Agent': 'Mozilla/5.0'})
-#     try:
-#         with urlopen(req) as conn:
-#             page = conn.read()
-#             soup = BeautifulSoup(page, features="html.parser")
-#             # print(soup.findAll('class="text_right wert selected"'))
-#             # print(soup)
-#             table = soup.find('table', attrs={'class': 'dynPageTable'})
-#             # print(table)
-#             rows = table.find('tr', {'class': 'dynPageTableLine1'})
-#             values = rows.find('td', {'class': 'text_right wert selected'})
-#             temp = re.search(r'([0-9]+.[0-9])', str(values))
-#             # print(float(temp.group()))
-#         try:
-#             temperature = float(temp.group())
-#         except ValueError as e:
-#             logger.exception(e)
-#             temperature = scrape_innerestadt()
-#         return temperature
-#     except URLError:
-#         return scrape_innerestadt()
-
-
-# def scrape_time():
-
-#     site = 'https://www.zamg.ac.at/cms/de/wetter/wetterwerte-analysen/wien'
-
-#     req = Request(site,  headers={
-#                   'User-Agent': 'Mozilla/5.0'})
-
-#     with urlopen(req) as conn:
-#         page = conn.read()
-#         soup = BeautifulSoup(page, features="html.parser")
-#         string = soup.find(
-#             'h2', {'class': 'dynPageTextHead float_left no_margin_bottom'})
-#         date = re.findall(
-#             r'Aktuelle Messwerte der Wetterstationen von ([0-9]{2})', str(string))
-#         # print(date[0])
-#         d = float(date[0])
-#         return d
+    site = "https://www.zamg.ac.at/cms/de/wetter/wetterwerte-analysen/wien"
+    return apply_scraping(site, scr_T)
